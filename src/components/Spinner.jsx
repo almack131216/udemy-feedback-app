8be76e0cddc8cd6754ec23447c1603e8f93b7b7a
ecp-alex <alex.mackenzie@englishcollege.cz>
2022-01-13 18:26:37 +0100

    UI | add Spinner (animated css)

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,14 @@
+function Spinner() {
+  return (
+    <div className='spinner-wrap'>
+      <div className='lds-ring'>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  )
+}
+
+export default Spinner
